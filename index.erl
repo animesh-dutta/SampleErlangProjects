791ca1aa2fe90a94b05c3119704bed759c0f6495
Animesh Dutta <34543420+animesh-dutta@users.noreply.github.com>
2025-02-04 02:16:17 +0530

    Update index.erl

--- index.erl
+++ index.erl
@@ -2,5 +2,5 @@ There are a lot of websites and apps which can be developed using erlang and it 
 If you want to become Chief Technology Officier,in the initial period you should do coding but if you are in higher rank in technology field,you
 should know different technologies and other things that is required.Those who are in higher rank,they don't have so much to do but should have
 knowledge so that any unprecedented events can be controlled by them.
-Female business magnates are the inspiration for male business magnates.                                                                                    
+Female business magnates are the inspiration for male business magnates.                                                                                     
   
