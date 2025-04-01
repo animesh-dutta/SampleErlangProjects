0715b4827f4b59fb805b7c9467dec41ec6ec4125
Animesh Dutta <34543420+animesh-dutta@users.noreply.github.com>
2025-04-01 18:26:19 +0530

    Update index.erl

--- index.erl
+++ index.erl
@@ -1,4 +1,4 @@
-<!--- Author:Animesh Dutta (Ananya Birla) --->  
+<!--- Author:Animesh Dutta (Ananya Birla) --->   
 There are a lot of websites and apps which can be developed using erlang and it is very important to upgrade youself with new technologies.
 If you want to become Chief Technology Officier,in the initial period you should do coding but if you are in higher rank in technology field,you
 should know different technologies and other things that is required.Those who are in higher rank,they don't have so much to do but should have
